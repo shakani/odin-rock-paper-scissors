9ef86c2c4b577bf84995fe4ec6087434ab6dea30
Sami Hakani <sami.hakani@gmail.com>
2023-08-09 14:08:30 -0700

    Added rps logic

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3); // returns randint from 0 to 2 inclusive; map to rock, paper, scissors resp.
+    let cs = Math.floor(Math.random() * 3); // returns randint from 0 to 2 inclusive; map to rock, paper, scissors resp.
+    if (cs === 0) {
+        return 'rock';
+    }
+    else if (cs === 1) {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
 }
+
+function toSentenceCase(s) {
+    return s[0].toUpperCase() + s.splice(1).toLowerCase();
+}
+
+function playRound(ps, computerSelection) {
+    let playerSelection = ps.toLowerCase(); // lower case for player selection
+
+    let win = false;
+
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    }
+    
+    // Win conditions
+    if (playerSelection === 'rock') {
+        win = (computerSelection === 'scissors');
+    }
+    if (playerSelection === 'paper') {
+        win = (computerSelection === 'rock');
+    }
+    if (playerSelection === 'scissors') {
+        win = (computerSelection === 'paper');
+    }
+
+    if (win) {
+        return 'You win! ' + toSentenceCase(playerSelection) + ' beats ' + toSentenceCase(computerSelection);
+    }
+    else {
+        return 'You lose! ' + toSentenceCase(computerSelection) + ' beats ' + toSentenceCase(playerSelection);
+    }
+}
+
